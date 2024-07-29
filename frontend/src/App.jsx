49e8ecbafeb29b2d0d77f09d752cba6e629bf3ea
aditya-1404 <aditya1442002@gmail.com>
2024-07-29 17:14:31 +0530

    browser router set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Home from "./components/home";
+import Nav from "./components/nav";
 function App() {
   return (
     <>
-
+    <Nav></Nav>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
