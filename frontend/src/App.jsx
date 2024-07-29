3d766e756d78e08cb2b4bb5dc147517d96928e0d
aditya-1404 <aditya1442002@gmail.com>
2024-07-29 19:57:29 +0530

    added guidex page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Nav from "./components/nav";
 import Lms from "./components/lms";
+import Guidex from "./components/guidex";
 import Footer from "./components/footer";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lms" element={<Lms />} />
+          <Route path="/guidex" element={<Guidex />} />
         </Routes>
       </BrowserRouter>
       <Footer></Footer>
