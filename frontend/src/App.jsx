070306d6f65ba282a5f2c911727cd74e18cb3fd8
aditya-1404 <aditya1442002@gmail.com>
2024-07-29 18:01:10 +0530

    home page designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes,Route } from "react-router-dom";
 import Home from "./components/home";
 import Nav from "./components/nav";
+import Footer from "./components/footer";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}/>
       </Routes>
     </BrowserRouter>
+    <Footer></Footer>
     </>
   );
 }
